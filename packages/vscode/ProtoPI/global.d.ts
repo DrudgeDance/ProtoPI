d86b61035164c2bd8307c5ec522a363ffa67c073
Adam D. <adam@adamduda.com>
2024-05-28 20:44:51 -0500

    update

---
+++ packages/vscode/ProtoPI/global.d.ts
@@ -0,0 +1,8 @@
+declare namespace WebAssembly {
+  interface Imports {}
+  interface Instance {}
+  interface Exports {}
+}
+
+declare class WebGLRenderingContext {}
+declare interface MessageEvent {}
